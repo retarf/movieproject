4b681d162eb07eb1b1c61cdec4c2da1ca8063a8b
retarf <retarf@gmail.com>
2019-11-01 13:16:58 +0100

    Urls update

--- django/moviesproject/urls.py
+++ django/moviesproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls')),
-    path('comments/', include('comments.urls')),
-    path('top/', include('top.urls')),
+    path('movies', include('movies.urls')),
+    path('comments', include('comments.urls')),
+    path('top', include('top.urls')),
 ]

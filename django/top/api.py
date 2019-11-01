060a1b14919a3617aec88de4c627c3d13a21109d
retarf <retarf@gmail.com>
2019-11-01 10:46:59 +0100

    Order added to top

--- django/top/api.py
+++ django/top/api.py
@@ -12,10 +12,14 @@ class TopView(APIView):
     def get(self, request, format=None):
         start_date = request.query_params['start_date']
         end_date = request.query_params['start_date']
-        movies = Comment.objects.filter(created__range=[start_date, end_date]).values('movie_id').annotate(total_comments=Count('movie_id'))
+        movies = Comment.objects.filter(created__range=[start_date, end_date]).values('movie_id').annotate(
+                total_comments=Count('movie_id')
+                ).order_by('-total_comments')
         number_of_comments = sorted(set(comments['total_comments'] for comments in movies), reverse=True)
         ranks = {}
-        for rank, number in enumerate(number_of_comments): ranks.update({number: rank + 1})
-        for movie in movies: movie.update({'rank': ranks[movie['total_comments']]})
+        for rank, number in enumerate(number_of_comments): 
+            ranks.update({number: rank + 1})
+        for movie in movies: movie.update({'rank': 
+            ranks[movie['total_comments']]})
 
         return Response(movies)

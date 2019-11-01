cb0d378885cd7492c008d8c4f8f3d15d87b93841
retarf <retarf@gmail.com>
2019-11-01 11:27:44 +0100

    Date filters added

--- django/top/api.py
+++ django/top/api.py
@@ -10,11 +10,31 @@ from comments.models import Comment
 class TopView(APIView):
 
     def get(self, request, format=None):
-        start_date = request.query_params['start_date']
-        end_date = request.query_params['start_date']
-        movies = Comment.objects.filter(created__range=[start_date, end_date]).values('movie_id').annotate(
-                total_comments=Count('movie_id')
-                ).order_by('-total_comments')
+        params = request.query_params
+        if params.get('start_date', None) is not None  and params.get('end_date', None) is not None:
+            start_date = request.query_params['start_date']
+            end_date = request.query_params['start_date']
+            movies = Comment.objects.filter(created__range=[start_date, end_date]).values('movie_id').annotate(
+                    total_comments=Count('movie_id')
+                    ).order_by('-total_comments')
+
+        elif params.get('start_date', None) is not None:
+            start_date = request.query_params['start_date']
+            movies = Comment.objects.filter(created__gte=start_date).values('movie_id').annotate(
+                    total_comments=Count('movie_id')
+                    ).order_by('-total_comments')
+
+        elif params.get('end_date', None) is not None:
+            end_date = request.query_params['start_date']
+            movies = Comment.objects.filter(created__lte=end_date).values('movie_id').annotate(
+                    total_comments=Count('movie_id')
+                    ).order_by('-total_comments')
+
+        else:
+            movies = Comment.objects.values('movie_id').annotate(
+                    total_comments=Count('movie_id')
+                    ).order_by('-total_comments')
+
         number_of_comments = sorted(set(comments['total_comments'] for comments in movies), reverse=True)
         ranks = {}
         for rank, number in enumerate(number_of_comments): 

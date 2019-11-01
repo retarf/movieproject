3101395e9ec6984a50110812af89563c9ede542c
retarf <retarf@gmail.com>
2019-11-01 21:00:57 +0100

    Heroku

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ ENV PYTHONBUFFERED 1
 COPY requirements.txt .
 RUN apt update \
     && apt upgrade -y 
-RUN apt install -y apt-utils python3-setuptools
+RUN apt install -y apt-utils python3-setuptools vim
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 RUN mkdir /project
--- django/movies/tests.py
+++ django/movies/tests.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
 
 class MovieTestCase(APITestCase):
     def test_url_root(self):
-        url = reverse('index')
+        url = reverse('movies:index')
         response = self.client.get(url)
         self.assertTrue(status.is_success(response.status_code))
+
 
 if __name__ == '__main__':
     unittest.main()

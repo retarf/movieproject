bf343ded4b4985dca7911b63cea2bf671d0cf191
retarf <retarf@gmail.com>
2019-11-03 09:44:21 +0100

    heroku db configuration

---
+++ django/moviesproject/heroku_db.py
@@ -0,0 +1,26 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd9174fhs2g91vi',
+        'USER': 'dxvgmlvnqumgxf',
+        'PASSWORD': 'dd3b7f9a66bff175c0b63c63de28dc9018a8996fbd591b61296f54aeacfecf38',
+        'HOST': 'ec2-107-22-216-53.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+'''
+Host
+    ec2-107-22-216-53.compute-1.amazonaws.com
+Database
+    d9174fhs2g91vi
+User
+    dxvgmlvnqumgxf
+Port
+    5432
+Password
+    dd3b7f9a66bff175c0b63c63de28dc9018a8996fbd591b61296f54aeacfecf38
+URI
+    postgres://dxvgmlvnqumgxf:dd3b7f9a66bff175c0b63c63de28dc9018a8996fbd591b61296f54aeacfecf38@ec2-107-22-216-53.compute-1.amazonaws.com:5432/d9174fhs2g91vi
+Heroku CLI
+    heroku pg:psql postgresql-tapered-59631 --app ldmovieproject
+'''
